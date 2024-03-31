0adb09a6bc96c18f15314d8d21d4e004685ca796
franmaranchello <fmaranchello@e-verse.com>
2024-03-31 18:30:32 -0300

    Contract and configuration

---
+++ contracts/Raffle.sol
@@ -0,0 +1,10 @@
+// Raffle contract
+
+// Enter
+// Pick a random winner
+// Winner to be selected every x minutes using Chainlink oracles
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Raffle {}
---
+++ hardhat.config.ts
@@ -0,0 +1,13 @@
+import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-gas-reporter";
+import "dotenv/config";
+import "solidity-coverage";
+import "hardhat-deploy";
+import { HardhatUserConfig } from "hardhat/config";
+
+module.exports = {
+  solidity: "0.8.7",
+};
